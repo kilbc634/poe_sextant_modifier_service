2bd054964e81823a5bf8a4a5a208c27e7c5837f5
kilbc634 <tuyn76801@gmail.com>
2023-09-28 00:45:17 +0800

    Add Celery test sample

---
+++ src/sextent_consumer.py
@@ -0,0 +1,24 @@
+from celery import Celery
+import time
+
+BROKER_URL = redis://:cverd6632@172.17.0.1:6379/0
+
+# 创建一个 Celery 实例
+app = Celery('schedule_sextent', broker=BROKER_URL)
+
+# 配置 Celery
+app.conf.task_routes = {
+    'my_task': {'queue': 'my_queue'}  # 指定任务队列
+}
+
+# 定义一个简单的任务
+@app.task
+def my_task(task_id):
+    print(f'Starting task {task_id}')
+    time.sleep(5)  # 模拟任务执行
+    print(f'Completed task {task_id}')
+    return f'Task {task_id} completed'
+
+# 启动 Celery worker
+if __name__ == '__main__':
+    app.worker_main()
---
+++ src/sextent_producer.py
@@ -0,0 +1,11 @@
+from sextent_consumer import my_task
+from celery import Celery
+
+BROKER_URL = redis://:cverd6632@172.17.0.1:6379/0
+
+# 创建一个 Celery 实例，以便与任务进行通信
+app = Celery('schedule_sextent', broker=BROKER_URL)
+
+# 向 Celery 发送任务
+task = my_task.apply_async(args=[1])  # 发送任务到队列
+print(f'Task ID: {task.id}')
