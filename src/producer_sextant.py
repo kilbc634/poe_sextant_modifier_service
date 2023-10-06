30b1045f4e680ffc415a894b35c478db204f500a
kilbc634 <tuyn76801@gmail.com>
2023-10-06 23:45:34 +0800

    update  runner

--- src/producer_sextant.py
+++ src/producer_sextant.py
@@ -11,51 +11,56 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__file__)
+import time
 
 
 def run_schedule():
-    # 從excel去load六分儀資料，並將資料解析成List格式
-    excelFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sextant_mapping.xlsx')
-    sextantData = get_sextant_data_from_excel(excelFile)
+    while True:
+        # 從excel去load六分儀資料，並將資料解析成List格式
+        excelFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sextant_mapping.xlsx')
+        sextantData = get_sextant_data_from_excel(excelFile)
 
-    logger.info('Start schedule when Excel Data loaded')
-    for data in sextantData:
-        # 遍歷list，產出第N筆資料對應的 statsList,dbKey
-        statsList = sextant_data_to_stats_list(data)
-        dbKey = sextant_data_to_db_key(data)
+        logger.info('Start schedule when Excel Data loaded')
+        for data in sextantData:
+            # 遍歷list，產出第N筆資料對應的 statsList,dbKey
+            statsList = sextant_data_to_stats_list(data)
+            dbKey = sextant_data_to_db_key(data)
 
-        # check該任務是否已經在queue，如果是那就直接skip
-        if check_sextant_task_pending(dbKey):
-            logger.info('Skip task produce when pending ({task})'.format(
-                task=dbKey)
-            )
-            continue
+            # check該任務是否已經在queue，如果是那就直接skip
+            if check_sextant_task_pending(dbKey):
+                logger.info('Skip task produce when pending ({task})'.format(
+                    task=dbKey)
+                )
+                continue
 
-        # 根據dbKey到DB取得最後更新時間
-        latestTime = get_sextant_latest_time(dbKey)
+            # 根據dbKey到DB取得最後更新時間
+            latestTime = get_sextant_latest_time(dbKey)
 
-        doTask = False
-        if latestTime:
-            latestTime = datetime.strptime(latestTime, '%Y-%m-%d %H:%M:%S')
-            nowTime = datetime.utcnow()
-            
-            # 如果當前時間和最後更新時間相差3hr以上，則安排task
-            if (nowTime - latestTime) > timedelta(hours=3):
+            doTask = False
+            if latestTime:
+                latestTime = datetime.strptime(latestTime, '%Y-%m-%d %H:%M:%S')
+                nowTime = datetime.utcnow()
+                
+                # 如果當前時間和最後更新時間相差3hr以上，則安排task
+                if (nowTime - latestTime) > timedelta(hours=3):
+                    doTask = True
+
+            # 如果latestTime不存在(dbKey還沒有資料)，也是安排task
+            else:
                 doTask = True
 
-        # 如果latestTime不存在(dbKey還沒有資料)，也是安排task
-        else:
-            doTask = True
 
+            if doTask:
+                task = trade_sextant_task.apply_async(args=[statsList, dbKey], queue=QueueTrade)
+                set_sextant_task_pending(dbKey, data={
+                    'taskId': task.id
+                })
+                logger.info('Task produce with ({task})'.format(
+                    task=dbKey)
+                )
 
-        if doTask:
-            task = trade_sextant_task.apply_async(args=[statsList, dbKey], queue=QueueTrade)
-            set_sextant_task_pending(dbKey, data={
-                'taskId': task.id
-            })
-            logger.info('Task produce with ({task})'.format(
-                task=dbKey)
-            )
+        # Event loop 間隔5分鐘
+        time.sleep(60 * 5)
 
 
 
--- src/run.py
+++ src/run.py
@@ -1,6 +1,26 @@
 import sys
-sys.path.append("./src")
+sys.path.append("..")
+from setting import *
 from producer_sextant import run_schedule
+import subprocess
+import time
 
 if __name__ == "__main__":
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    commandRunConsumer = 'python3 consumer.py'
+    processRunConsumer = subprocess.Popen(commandRunConsumer, shell=True, cwd=current_directory)
+    print('Consumer is running...')
+
+    time.sleep(10)
+
+    commandRunFlower = 'celery --broker={broker} flower'.format(
+        broker=BROKER_URL
+    )
+    processRunFlower = subprocess.Popen(commandRunFlower, shell=True, cwd=current_directory)
+    print('Flower is running...')
+
+    time.sleep(10)
+
+    print('Start event loop schedule......')
     run_schedule()
